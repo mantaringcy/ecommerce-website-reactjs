dc99de9e6d77106ff472b4c9aaddf9d6f53b0a15
Cymon Mantaring <mantaringcy23@gmail.com>
2026-02-12 23:30:10 +0800

    Added HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,27 @@
+import ProductCard from "../components/ProductCard";
+import { getProducts } from "../data/product";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const products = getProducts();
+
+  return (
+    <div className="page">
+      <div className="home-hero">
+        <h1 className="home-title">Welcome to Shophub</h1>
+        <p className="home-subtitle">
+          Disover amazing products at great prices
+        </p>
+      </div>
+
+      <div className="container">
+        <h2 className="page-title">Our Products</h2>
+        <div className="product-grid">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HomePage;
